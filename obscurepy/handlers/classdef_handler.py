073d710c608d0f170ea77c9cf5d3166a047d2592
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2021-05-22 20:16:43 -0600

    Made dict methods in ClassDefHandler static functions and added tests for them

--- obscurepy/handlers/classdef_handler.py
+++ obscurepy/handlers/classdef_handler.py
@@ -4,6 +4,90 @@ from obscurepy.handlers.handler import Handler
 from obscurepy.utils.definition_tracker import DefinitionTracker
 
 
+def get_base_classes(node):
+    """An ugly function that gets a list of classes inhereted from
+
+    Args:
+        **node (:obj: `ast.ClassDef`)**: The ClassDef node for which to get the base classes of
+
+    Returns:
+        List of base classes (str), or empty list if none
+    """
+    bases = []
+    if type(node) == ast.ClassDef:
+        for base in node.bases:
+            bases.append(base.id)
+    return bases
+
+
+def get_methods(node):
+    """An ugly function that gets a list of class methods
+
+    Args:
+        **node (:obj: `ast.ClassDef`)**: The ClassDef node for which to get the methods of
+
+    Returns:
+        List of class methods (str), or empty list if none
+    """
+    methods = []
+    for method in node.body:
+        if type(method) == ast.FunctionDef:
+            methods.append(method.name)
+    return methods
+
+
+def get_properties(node):
+    """An ugly function that gets a list of class properties
+
+    Args:
+        **node (:obj: `ast.ClassDef`)**: The ClassDef node for which to get the properties of
+
+    Returns:
+        List of class properties (str), or empty list if none
+    """
+    properties = []
+    for method in node.body:
+        if type(method) == ast.FunctionDef:
+            # This needs to check for all 'self' properties no just those in __init__
+            if method.name == '__init__':
+                for assign in method.body:
+                    if type(assign) == ast.Assign:
+                        for target in assign.targets:
+                            if target.value.id == 'self':
+                                properties.append(target.attr)
+    return properties
+
+
+def get_variables(node):
+    """An ugly function that gets a list of class variables
+
+    Args:
+        **node (:obj: `ast.ClassDef`)**: The ClassDef node for which to get the variables of
+
+    Returns:
+        List of class variables (str), or empty list if none
+    """
+    variables = []
+    for variable in node.body:
+        if type(variable) == ast.Assign:
+            for target in variable.targets:
+                if type(target) == ast.Name:
+                    variables.append(target.id)
+    return variables
+
+
+def create_class_dictionary(node):
+    class_dict = {
+        'new_name': "",
+        'prev_name': node.name,
+        'variables': get_variables(node),
+        'properties': get_properties(node),
+        'methods': get_methods(node),
+        'bases': get_base_classes(node)
+    }
+    return class_dict
+
+
 class ClassDefHandler(Handler):
     """Class to traverse and modify ClassDef nodes in an ast
 
@@ -31,84 +115,7 @@ class ClassDefHandler(Handler):
         """
         tracker = DefinitionTracker.get_instance()
         if isinstance(node.name, str):
-            class_dict = self._create_class_dictionary(node)
+            class_dict = create_class_dictionary(node)
             tracker.add_class(class_dict)
             node.name = tracker.definitions['classes'][node.name]['new_name']
         return node
-
-    def _create_class_dictionary(self, node):
-        class_dict = {
-            'new_name': "",
-            'prev_name': node.name,
-            'variables': self._get_variables(node),
-            'properties': self._get_properties(node),
-            'methods': self._get_methods(node),
-            'bases': self._get_base_classes(node)
-        }
-        return class_dict
-
-    def _get_variables(self, node):
-        """An ugly function that gets a list of class variables
-
-        Args:
-            **node (:obj: `ast.ClassDef`)**: The ClassDef node for which to get the variables of
-
-        Returns:
-            List of class variables (str), or empty list if none
-        """
-        variables = []
-        for variable in node.body:
-            if type(variable) == ast.Assign:
-                for target in variable.targets:
-                    if type(target) == ast.Name:
-                        variables.append(target.id)
-        return variables
-
-    def _get_properties(self, node):
-        """An ugly function that gets a list of class properties
-
-        Args:
-            **node (:obj: `ast.ClassDef`)**: The ClassDef node for which to get the properties of
-
-        Returns:
-            List of class properties (str), or empty list if none
-        """
-        properties = []
-        for method in node.body:
-            if type(method) == ast.FunctionDef:
-                if method.name == '__init__':
-                    for assign in method.body:
-                        if type(assign) == ast.Assign:
-                            for target in assign.targets:
-                                if target.value.id == 'self':
-                                    properties.append(target.attr)
-        return properties
-
-    def _get_methods(self, node):
-        """An ugly function that gets a list of class methods
-
-        Args:
-            **node (:obj: `ast.ClassDef`)**: The ClassDef node for which to get the methods of
-
-        Returns:
-            List of class methods (str), or empty list if none
-        """
-        methods = []
-        for method in node.body:
-            if type(method) == ast.FunctionDef:
-                methods.append(method.name)
-        return methods
-
-    def _get_base_classes(self, node):
-        """An ugly function that gets a list of classes inhereted from
-
-        Args:
-            **node (:obj: `ast.ClassDef`)**: The ClassDef node for which to get the base classes of
-
-        Returns:
-            List of base classes (str), or empty list if none
-        """
-        bases = []
-        for base in node.bases:
-            bases.append(base.id)
-        return bases
--- tests/test_classdef_handler.py
+++ tests/test_classdef_handler.py
@@ -2,6 +2,11 @@ import ast
 import unittest
 from obscurepy.handlers.classdef_handler import ClassDefHandler
 from obscurepy.utils.definition_tracker import DefinitionTracker
+from obscurepy.handlers.classdef_handler import create_class_dictionary
+from obscurepy.handlers.classdef_handler import get_base_classes
+from obscurepy.handlers.classdef_handler import get_methods
+from obscurepy.handlers.classdef_handler import get_properties
+from obscurepy.handlers.classdef_handler import get_variables
 
 
 class ClassDefHandlerTest(unittest.TestCase):
@@ -10,9 +15,59 @@ class ClassDefHandlerTest(unittest.TestCase):
         self.fixture = ClassDefHandler()
         self.tracker = DefinitionTracker.get_instance()
         self.tracker.clear_definitions()
-        self.source = 'class FirstClass:\n\tpass\n\nclass SecondClass:\n\tpass'
+        self.source = 'class TestClass(BaseClass):\n\t' \
+                      'eighty_four = 84\n\t' \
+                      'def __init__(self):\n\t\t' \
+                      'self.forty_two = 42'
+        self.tree = ast.parse(self.source)
 
     def test_visitClassDef(self):
-        tree = ast.parse(self.source)
-        tree = self.fixture.handle(tree)
-        self.assertEqual(len(self.tracker.definitions['classes']), 2)
+        self.tree = self.fixture.handle(self.tree)
+        self.assertEqual(len(self.tracker.definitions['classes']), 1)
+
+    def test_create_class_dictionary(self):
+        class_dict = create_class_dictionary(self.tree.body[0])
+        self.tracker.add_class(class_dict)
+        class_ = self.tracker.definitions['classes']['TestClass']
+        self.assertEqual(class_['new_name'], '_0x397')
+        self.assertEqual(class_['prev_name'], 'TestClass')
+        self.assertEqual(class_['variables'][0], 'eighty_four')
+        self.assertEqual(class_['properties'][0], 'forty_two')
+        self.assertEqual(class_['methods'][0], '__init__')
+        self.assertEqual(class_['bases'][0], 'BaseClass')
+
+    def test_get_variables(self):
+        variables = get_variables(self.tree.body[0])
+        self.assertEqual(variables[0], 'eighty_four')
+
+    def test_get_variables_none(self):
+        tree = ast.parse('class TestClass:\n\tpass')
+        variables = get_variables(tree)
+        self.assertTrue(len(variables) == 0)
+
+    def test_get_properties(self):
+        properties = get_properties(self.tree.body[0])
+        self.assertEqual(properties[0], 'forty_two')
+
+    def test_get_properties_none(self):
+        tree = ast.parse('class TestClass:\n\tpass')
+        properties = get_properties(tree)
+        self.assertTrue(len(properties) == 0)
+
+    def test_get_methods(self):
+        methods = get_methods(self.tree.body[0])
+        self.assertEqual(methods[0], '__init__')
+
+    def test_get_methods_none(self):
+        tree = ast.parse('class TestClass:\n\tpass')
+        methods = get_methods(tree)
+        self.assertTrue(len(methods) == 0)
+
+    def test_get_base_classes(self):
+        bases = get_base_classes(self.tree.body[0])
+        self.assertEqual(bases[0], 'BaseClass')
+
+    def test_get_base_classes_none(self):
+        tree = ast.parse('class TestClass:\n\tpass')
+        bases = get_base_classes(tree)
+        self.assertTrue(len(bases) == 0)
