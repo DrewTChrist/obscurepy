84091ce02ac5f539120324a736fece885c26a2c5
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2021-05-24 20:14:34 -0600

    Added tests for obscure.py

--- obscurepy/scripts/obscure.py
+++ obscurepy/scripts/obscure.py
@@ -1,6 +1,5 @@
 from obscurepy.obfuscator import Obfuscator
 import click
-import sys
 
 
 @click.command()
@@ -9,11 +8,13 @@ import sys
 @click.option('--project_dir', help='The directory of the project to obscure')
 @click.option('--output_dir', default='.', help='The directory to output the obscured code')
 def obscure(filepath, project, project_dir, output_dir):
+    """Accepts command line arguments and passes them to the Obfuscator"""
     check_bad_option_usage(filepath, project, project_dir, output_dir)
     Obfuscator(filepath, project, project_dir, output_dir).obscure()
 
 
 def check_bad_option_usage(filepath, project, project_dir, output_dir):
+    """Validates the arguments passed to the Obfuscator"""
     if filepath and project:
         raise click.BadOptionUsage('project',
                                    'You should not enable the project flag if filepath is provided')
@@ -26,7 +27,6 @@ def check_bad_option_usage(filepath, project, project_dir, output_dir):
     elif not project and project_dir:
         raise click.BadOptionUsage('project',
                                    'If a project directory is provided the project flag must be enabled')
-
-
-if __name__ == '__main__':
-    obscure()
+    elif not filepath and not project and not project_dir and (output_dir == '.'):
+        raise click.MissingParameter(
+            'You must provide some arguments to obscure')
